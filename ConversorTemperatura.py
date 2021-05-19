a1d96d869bfac106dc847249bb37227a5876b034
Felipe Machado <felipedem.machado@gmail.com>
2021-05-19 18:15:54 -0300

    Primeiro Commit

---
+++ ConversorTemperatura.py
@@ -0,0 +1,6 @@
+
+temperaturaF = input("Digite a temperatura em Fahrenheit: ")
+F = float(temperaturaF)
+temperaturaC = (F-32)*5/9
+C = int(temperaturaC)
+print("A temperatura em Celsius é aproximadamente:",C)
