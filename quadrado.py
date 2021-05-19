c3cd2d3ab35df67a4fab276d4dfc69da42ef99a9
Felipe Machado <felipedem.machado@gmail.com>
2021-05-19 18:19:47 -0300

    Primeiro Commit

---
+++ quadrado.py
@@ -0,0 +1,6 @@
+ladoDoQuadrado = int(input("Digite o valor correspondente ao lado de um quadrado:"))
+
+area = ladoDoQuadrado**2
+perimetro = ladoDoQuadrado*4
+
+print("perímetro:", perimetro, "- área:", area)
